b5ba239cc275e283b800f301ed102cf71f38eb3c
EvgeniyHalimon <halimonevgeniy@gmail.com>
2021-12-13 18:20:40 +0200

    added action-creators, form, constants

--- src/store/actions/action-creators.ts
+++ src/store/actions/action-creators.ts
@@ -1,2 +1,31 @@
-export {}
-const q: string = ''
+import axios from "axios"
+import { Dispatch } from "redux"
+import { KEY, URL } from "../../constants/constants"
+import { Action } from "../type"
+import { ForecastActionType } from "./action-types"
+
+export const setQuery = (str: string) => {
+    return{
+        type: ForecastActionType.SET_QUERY,
+        payload: str
+    }
+}
+
+export const setForecast = (obj: object) => {
+    return{
+        type: ForecastActionType.SET_FORECAST,
+        payload: obj
+    }
+}
+
+export const fetchForecast = (query: string) => {
+    return async(dispatch: Dispatch) => {
+        try{
+            const forecastData = await axios.get(`${URL}?q=${query}&units=metric&appid=${KEY}`)
+            console.log(forecastData.data);
+            dispatch(setForecast(forecastData.data))
+        } catch(err){
+            console.log(err);
+        }
+    }
+}
